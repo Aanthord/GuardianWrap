ef265ea007a26442befad24d6411d5e6c06e8300
Aanthord <michael.doran.808@gmail.com>
2024-02-28 15:12:23 +0100

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,15 @@
 #include "utils.h"
+#include "logger.h" // Include logger for error logging
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 char* bytes_to_hex(const uint8_t* bytes, size_t len) {
     char* hex_string = malloc((len * 2) + 1);
+    if (hex_string == NULL) {
+        log_message(LOG_LEVEL_ERROR, "Memory allocation failed in bytes_to_hex.");
+        return NULL;
+    }
     for (size_t i = 0; i < len; i++) {
         sprintf(hex_string + (i * 2), "%02x", bytes[i]);
     }
@@ -15,9 +20,8 @@ char* bytes_to_hex(const uint8_t* bytes, size_t len) {
 void register_signal_handlers(void (*handler)(int)) {
     // Example for SIGINT
     if (signal(SIGINT, handler) == SIG_ERR) {
-        fprintf(stderr, "Cannot handle SIGINT!\n");
+        log_message(LOG_LEVEL_ERROR, "Cannot handle SIGINT!");
         exit(EXIT_FAILURE);
     }
     // Additional signals could be registered here
 }
-
