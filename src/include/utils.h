9de94822f0f2262f7678be731bde50a3e7f46b7d
Aanthord <michael.doran.808@gmail.com>
2024-02-29 11:24:45 +0100

    Update utils.h

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,8 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+// Function declarations for utility functions
 char* bytes_to_hex(const uint8_t* bytes, size_t len);
 void register_signal_handlers(void (*handler)(int));
 
 #endif // UTILS_H
-
